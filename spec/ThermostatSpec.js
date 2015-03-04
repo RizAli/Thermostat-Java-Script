73e13151f3356b0e817af69e2e1e55f7a856e844
Rizwan Ali <rizcube@gmail.com>
2015-03-04 16:57:06 +0000

    added reset button, max value of PSM

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,15 +1,15 @@
 describe('Thermostat', function() {
   var thermostat;
 
-  beforeEach(function(){
+  beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it('has a default temperature of 20 degrees', function(){
+  it('has a default temperature of 20 degrees', function() {
     expect(thermostat.currentTemperature).toEqual(20);
   });
 
-  it('can increase the temperature by one degrees', function(){
+  it('can increase the temperature by one degrees', function() {
     thermostat.increaseTemperature(1);
     expect(thermostat.currentTemperature).toEqual(21);
   });
@@ -24,9 +24,35 @@ describe('Thermostat', function() {
     expect(thermostat.currentTemperature).toEqual(19);
   });
 
-  it('can decrease the temperature by 5 degrees', function(){
+  it('can decrease the temperature by 5 degrees', function() {
     thermostat.decreaseTemperature(5);
     expect(thermostat.currentTemperature).toEqual(15);
+  });
+
+  it('has a minimum temperature of 10 degrees', function() {
+    thermostat.decreaseTemperature(16);
+    expect(thermostat.currentTemperature).toEqual(10);
+  });
+
+  it('has a power saving mode ON by default', function() {
+    // thermostat.PSM = true;
+    expect(thermostat.PSM).toEqual(true);
+  });
+
+  it('can be set to OFF', function() {
+    thermostat.PSMOnOff(false);
+    expect(thermostat.PSM).toEqual(false);
+  });
+
+  it('has a maxmimum temp of 25 degrees when PSM is ON', function() {
+    thermostat.increaseTemperature(20);
+    expect(thermostat.currentTemperature).toEqual(25);
+  });
+
+  it('can reset the temperature when clicked on the reset button', function() {
+    thermostat.increaseTemperature(5);
+    thermostat.resetBtn();
+    expect(thermostat.currentTemperature).toEqual(20);
   });
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,35 @@
 var Thermostat = function() {
     this.currentTemperature = 20;
+    this.PSM = true;
 };
 
 Thermostat.prototype.increaseTemperature = function(degrees){
   this.currentTemperature += degrees;
+  if (this.PSM) {
+      this.currentTemperature = Math.min(25, this.currentTemperature);
+  }
 };
 
 Thermostat.prototype.decreaseTemperature = function(degrees){
   this.currentTemperature -= degrees;
+  this.currentTemperature = Math.max(10, this.currentTemperature);
 };
+
+
+Thermostat.prototype.PSMOnOff = function(mode){
+  this.PSM = false;
+};
+
+Thermostat.prototype.resetBtn = function(){
+  this.currentTemperature = 20;
+};
+
+
+
+
+// //   return this.currentTemperature < 10
+// //   // if ( this.currentTemperature < 10) { return this.currentTemperature === 10; }
+
+
+// // this.currentTemperature  = (condition) ? true : false
+// // this.curre
